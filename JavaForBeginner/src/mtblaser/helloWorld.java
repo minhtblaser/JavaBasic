3521ee33e5fc2295a79ac3f3ffd731111fc25b55
minhtblaser <trinhngocminh,9a1@gmail.com>
2022-02-14 23:28:44 +0700

    setting

---
+++ JavaForBeginner/src/mtblaser/helloWorld.java
@@ -0,0 +1,7 @@
+package mtblaser;
+
+public class helloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ JavaForBeginner/src/mtblaser/koo.java
@@ -0,0 +1,13 @@
+package mtblaser;
+
+public class koo {
+ //okie hiêu
+// sao nó rắc rồi thế 
+	// nó hơn vscode chỗ nào ô
+	//kiểu nó chỉ dành cho java thui á ông
+	//okiee, kì này làm bài tập lớn môn java
+	// chung nhóm mới khỏe 
+	// chứ khác là die 
+	// để mai fix lại c++ thử 
+	//để chỉ ông push lên git
+}
